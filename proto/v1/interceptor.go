2993fb6c9441fb6c50bef8ff743737cebae67a83
kei6u <keisuke.umegaki.630@gmail.com>
2021-12-02 08:26:32 +0900

    update error message

--- proto/v1/interceptor.go
+++ proto/v1/interceptor.go
@@ -13,10 +13,10 @@ func hclogUnaryInterceptor(l hclog.Logger) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 		res, err := handler(ctx, req)
 		if err != nil {
-			l.Error(fmt.Sprintf("%s is failed with %s", info.FullMethod, status.Code(err).String()), "error", err)
+			l.Error(fmt.Sprintf("%s fails with %s", info.FullMethod, status.Code(err).String()), "error", err)
 			return nil, err
 		}
-		l.Info(fmt.Sprintf("%s is finished with %s", info.FullMethod, status.Code(err).String()))
+		l.Info(fmt.Sprintf("%s has finished with %s", info.FullMethod, status.Code(err).String()))
 		return res, nil
 	}
 }
